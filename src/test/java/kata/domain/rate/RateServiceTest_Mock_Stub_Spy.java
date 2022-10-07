f1e3d497b538efc73c51006f2871645f44a9898d
Damien Menanteau <damien.menanteau@gmail.com>
2022-10-07 16:28:25 +0200

    Squash Mock, Spy and Stub test classes into a single one

---
+++ src/test/java/kata/domain/rate/RateServiceTest_Mock_Stub_Spy.java
@@ -0,0 +1,208 @@
+package kata.domain.rate;
+
+import kata.domain.film.Film;
+
+import static kata.domain.film.FilmDummy.randomFilm;
+
+import kata.domain.film.FilmService;
+
+import static kata.domain.rate.RateDummy.*;
+
+import kata.domain.user.UserId;
+import kata.domain.user.UserIdDummy;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Optional;
+
+class RateServiceTest_Mock_Stub_Spy {
+    private RateRepository repository;
+    private FilmService filmService;
+    private RateService rateService;
+    private LikedNotifier likedNotifier;
+
+    @BeforeEach
+    void setup() {
+        repository = Mockito.mock(RateRepository.class);
+        filmService = Mockito.mock(FilmService.class);
+        likedNotifier = Mockito.mock(LikedNotifier.class);
+        rateService = new RateService(repository, filmService, likedNotifier);
+    }
+
+    @Test
+    void shouldSaveInTheRepository_usingASpy() {
+        final Rate rate = Rate.of("aTitle", 4, UserIdDummy.randomUserId());
+
+        // Exercise
+        rateService.save(rate);
+
+        // Verify expectations
+    }
+
+    @Test
+    void shouldReceiveFromRepository_usingAStub() {
+        final Rate rate = Rate.of("aTitle", 4, UserIdDummy.randomUserId());
+
+        // Setup
+
+        // Exercise
+        final Optional<Rate> ratingFromRepo = rateService.findById(rate.id);
+
+        // Verify State
+    }
+
+    @Test
+    void shouldReceiveFromRepository_usingAMock() {
+        final Rate rate = Rate.of("aTitle", 4, UserIdDummy.randomUserId());
+
+        // Setup Expectations
+
+        // Exercise
+        final Optional<Rate> ratingFromRepo = rateService.findById(rate.id);
+
+        // Verify expectations
+
+        // Verify State
+    }
+
+    @Test
+    void shouldReturnRatesMadeByAUser_usingAStub() {
+        final UserId userId = UserId.of("aUser");
+        final Rate rateOneByUser = randomRate().withUserId(userId).build();
+        final Rate rateTwoByUser = randomRate().withUserId(userId).build();
+
+        final List<Rate> allRates = randomListOfRatesOfSize(10);
+        allRates.add(rateOneByUser);
+        allRates.add(rateTwoByUser);
+
+        // Setup
+
+        // Exercise
+        final List<Rate> ratedByUser = rateService.findByUser(userId);
+
+        // Verify State
+    }
+
+    @Test
+    void shouldReturnRatesMadeByAUser_usingAMock() {
+        final UserId userId = UserId.of("aUser");
+        final Rate rateOneByUser = randomRate().withUserId(userId).build();
+        final Rate rateTwoByUser = randomRate().withUserId(userId).build();
+
+        final List<Rate> allRates = randomListOfRatesOfSize(10);
+        allRates.add(rateOneByUser);
+        allRates.add(rateTwoByUser);
+
+        // Setup Expectations
+
+        // Exercise
+        final List<Rate> ratedByUser = rateService.findByUser(userId);
+
+        // Verify expectations
+
+        // Verify State
+    }
+
+    @Test
+    void shouldReturnTheListOfRatesByAUserForAFilmThatWasProducedAtYearOrMoreRecent_usingAStub() {
+        final UserId userId = UserId.of("aUser");
+        final int productionYear = 2000;
+
+        final String theLionKingTitle = "The Lion King";
+        final Film theLionKingMovieAsOldFilm = randomFilm()
+                .withTitle(theLionKingTitle)
+                .withReleaseDate(1994)
+                .build();
+        final String frozenTitle = "Frozen";
+        final Film frozenMovieAsNewerFilm = randomFilm()
+                .withTitle(frozenTitle)
+                .withReleaseDate(2013)
+                .build();
+        final Rate rateOfFrozenByUser = randomRate()
+                .withTitle(frozenTitle)
+                .withUserId(userId)
+                .build();
+        final Rate rateOfTheLionKingByUser = randomRate()
+                .withTitle(theLionKingTitle)
+                .withUserId(userId)
+                .build();
+        final List<Rate> allRates = randomListOfRatesOfSize(10);
+        allRates.add(rateOfFrozenByUser);
+        allRates.add(rateOfTheLionKingByUser);
+
+        // Setup
+
+        // Exercise
+        final List<Rate> ratesByUserOfFilmsMadeAtYear2000OrMoreRecent = rateService
+                .ratedByUserAtYearOrMoreRecent(userId, productionYear);
+
+        // Verify State
+    }
+
+    @Test
+    void shouldReturnTheListOfRatesByAUserForAFilmThatWasProducedAtYearOrMoreRecent_usingAMock() {
+        final UserId userId = UserId.of("aUser");
+        final int productionYear = 2000;
+
+        final String theLionKingTitle = "The Lion King";
+        final Film theLionKingMovieAsOldFilm = randomFilm()
+                .withTitle(theLionKingTitle)
+                .withReleaseDate(1994)
+                .build();
+        final String frozenTitle = "Frozen";
+        final Film frozenMovieAsNewerFilm = randomFilm()
+                .withTitle(frozenTitle)
+                .withReleaseDate(2013)
+                .build();
+        final Rate rateOfFrozenByUser = randomRate()
+                .withTitle(frozenTitle)
+                .withUserId(userId)
+                .build();
+        final Rate rateOfTheLionKingByUser = randomRate()
+                .withTitle(theLionKingTitle)
+                .withUserId(userId)
+                .build();
+        final List<Rate> allRates = randomListOfRatesOfSize(10);
+        allRates.add(rateOfFrozenByUser);
+        allRates.add(rateOfTheLionKingByUser);
+
+        // Setup Expectations
+
+        // Exercise
+        final List<Rate> ratesByUserOfFilmsMadeAtYear2000OrMoreRecent = rateService
+                .ratedByUserAtYearOrMoreRecent(userId, productionYear);
+
+        // Verify expectations
+
+        // Verify State
+    }
+
+    @Test
+    void whenAFilmIsRatedMoreThan10Times_ItShouldNotifyOnceThatItHasBeenLikedBy10DifferentUsers_usingASpy() {
+        final String title = "aTitle";
+        final List<Rate> ratesForFilm = randomListOfRatesOfSizeForFilm(RateService.RATES_PER_FILM_FOR_NOTIFICATION, title);
+
+        // Setup
+
+        // Exercise
+        rateService.save(randomRate().withTitle(title).build());
+
+        // Verify it has been called
+    }
+
+    @Test
+    void whenAFilmIsRatedMoreThan10Times_ItShouldNotifyOnceThatItHasBeenLikedBy10DifferentUsers_usingAMock() {
+        final String title = "aTitle";
+        final Rate rate = randomRate().withTitle(title).build();
+        final List<Rate> ratesForFilm = randomListOfRatesOfSizeForFilm(RateService.RATES_PER_FILM_FOR_NOTIFICATION, title);
+
+        // Setup Expectations
+
+        // Exercise
+        rateService.save(rate);
+
+        // Verify expectations
+    }
+}
